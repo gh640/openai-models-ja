cba49b205cf34c19696a527f51d579803c0cdaa1
Goto Hayato <gh640@hey.com>
2026-02-11 11:59:38 +0900

    `list_models.sh` をディレクトリがすでにある場合でも処理を続けるように変更

--- list_models.sh
+++ list_models.sh
@@ -2,7 +2,7 @@
 set -eux
 
 PARENT_DIR=$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)
-DATE="$(date +%Y%m%d)"
-DATA_DIR="./scripts/out/${DATE}"
-uv run --directory "${PARENT_DIR}" mkdir "${DATA_DIR}"
+DATE="${DATE:-$(date +%Y%m%d)}"
+DATA_DIR="${PARENT_DIR}/scripts/out/${DATE}"
+mkdir -p "${DATA_DIR}"
 uv run --directory "${PARENT_DIR}" ./scripts/list_models.py > "${DATA_DIR}/models.csv"
